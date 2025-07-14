cf4b9fac5c10ba884ffe207568fc08b4cec7ebd6
Yehonatan-Bar <yonzbar@gmail.com>
2025-07-14 18:12:25 +0300

    doumentations

--- StudentManagementAPI/obj/Debug/net8.0/StudentManagementAPI.AssemblyInfo.cs
+++ StudentManagementAPI/obj/Debug/net8.0/StudentManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1ed6e3ab359c1492af15c8ad3dc71062a3f7d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
